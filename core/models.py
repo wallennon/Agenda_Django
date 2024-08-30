3bf6a2cee86076f3c55e48bbab43f5d26a19be03
wallennon <wallennon@gmail.com>
2024-08-30 15:59:37 -0300

    Criando tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -8,12 +8,16 @@ class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
-    data_criacao = models.DateTimeField(auto_now=True, verbose_name='Data de Criação do Evento')
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE) #atribuindo evento ao User e deletando suas ações
-
+    data_criacao = models.DateTimeField(
+        # atribuindo evento ao User e deletando suas ações
+        auto_now=True, verbose_name='Data de Criação do Evento')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'evento'
 
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%y às %H:%M hrs')
--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Evento
 
 # Create your views here.
+
+#redirecionando a página principal através da URL
+# def index(request):
+#     return redirect('/agenda/')
+
+
+def lista_eventos(request):
+    # para coletar o evento direto do usuário logado
+    # usuario = request.user
+    # evento = Evento.objects.filter(usuario=usuario)
+    evento = Evento.objects.all()  # aqui é passado todos os eventos listados no bd
+    dados = {'eventos': evento}
+    return render(request, 'agenda.html', dados)
